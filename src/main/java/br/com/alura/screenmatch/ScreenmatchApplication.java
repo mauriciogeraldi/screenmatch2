fc18e681e2fe71de9af497487994fefbac7c38c6
mauriciogeraldi <ma.uricio@hotmail.com>
2025-01-09 21:36:02 +0100

    Inicio do Projeto Spring

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -9,6 +9,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
 
@@ -30,10 +33,13 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
 		System.out.println(dadosEpisodio);
 
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
 		for (int i=1; i<=dados.totalTemporadas(); i++) {
 			json = consumoApi.obterDados("https://omdbapi.com/?t=gilmore+girls&Season="+i+"&apikey=ee24111f");
 			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-			System.out.println(dadosTemporada);
+			temporadas.add(dadosTemporada);
 		}
+		temporadas.forEach(System.out::println);
 	}
 }
